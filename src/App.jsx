4a186b6c789dcd251454662c6a7093fa196b2ac8
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2024-08-16 18:57:26 +0000

    added and updated some features

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar'; // Import Navbar as default
 import SliderTab from './Components/SliderTab/SliderTab';
 import Footer from './Components/Footer/footer';
+import Intro from './Components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      
       <Hero/>
       <br></br>
+      
+      <Intro />
+
       <SliderTab />
       <br></br>
+      
       <Footer />
     </div>
   );
