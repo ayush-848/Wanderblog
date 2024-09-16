5482136b4862d5929ea56ef65ddc95e67f757741
Ayush Deb <ayushdeb848@gmail.com>
2024-09-16 21:44:57 +0530

    updated

---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+// backend/models/User.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  password: { type: String, required: true },
+  profilePic: { type: String }, // URL or filename of the profile picture
+});
+
+// Password hashing before saving
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
