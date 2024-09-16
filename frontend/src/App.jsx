34fb8f415273e9bcfe6a7e4b5943b407613becc7
Ayush Deb <ayushdeb848@gmail.com>
2024-09-17 00:28:04 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar'; // Import Navbar as default
 import SliderTab from './Components/SliderTab/SliderTab';
-import Footer from './Components/Footer/Footer';
 import Intro from './Components/Intro/Intro';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
