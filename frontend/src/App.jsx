f38d5ee1b6fd66bfc2a00769ebadf46f70f26e20
Ayush Deb <ayushdeb848@gmail.com>
2024-09-17 12:10:30 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar'; // Import Navbar as default
 import SliderTab from './Components/SliderTab/SliderTab';
 import Intro from './Components/Intro/Intro';
-import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
       <Intro />
       <SliderTab />
       <br />
-      <Footer />
     </div>
   );
 }
