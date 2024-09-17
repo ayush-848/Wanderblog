d86ae66f2e9ed8f0a6e98f2b6ceba8d6b6cce126
Ayush Deb <ayushdeb848@gmail.com>
2024-09-17 14:12:26 +0530

    updated

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { API_URL } from '../config';
+import { API_URL } from '../../config';
 
 export const AuthContext = createContext(null);
 
