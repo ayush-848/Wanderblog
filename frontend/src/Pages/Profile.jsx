89aa4e2d5379b1764ca2609a09c44a1b5c0a5356
Ayush Deb <ayushdeb848@gmail.com>
2024-09-24 10:35:19 +0530

    updated

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from '../Components/Navbar/Navbar';
-import DashBoard from '../Components/DashBoard/DashBoard';
+import Navbar from '../Components/Navbar';
+import DashBoard from '../Components/Dashboard';
 
 function Profile(){
     return (
