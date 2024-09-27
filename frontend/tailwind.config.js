95d7b195ff8a1edbc7d084fb19c827435f772de8
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2024-09-27 16:40:56 +0000

    Added css for signup page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Outfit:['Outfit'],
+        Fredoka:['Fredoka'],
+      },
+    },
   },
   plugins: [],
 }
