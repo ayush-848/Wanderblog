7e7c80a14f9d9c23442e5e959a5bb834e2495b0b
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2025-01-22 21:33:50 +0530

    integrated clerk auth

--- frontend/src/Components/ProfileMenu.jsx
+++ frontend/src/Components/ProfileMenu.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CgProfile } from "react-icons/cg";
-import useAuth from '../hooks/useAuth';
 import { FaCog, FaSignOutAlt, FaUser, FaPen } from 'react-icons/fa';
-import Logout from "./../Pages/Auth/Logout";
-
+import Logout from "../Pages/Logout";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, useUser } from '@clerk/clerk-react';
 
 function ProfileMenu() {
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  const { isAuthenticated, user } = useAuth();
+  const { user } = useUser();
 
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
@@ -20,7 +19,7 @@ function ProfileMenu() {
   const handleCloseLogout = () => setOpenLogout(false);
 
 
-  return (  
+  return (
     <div className="relative flex items-center cursor-pointer">
       {user?.profilePic ? (
         <img
@@ -34,41 +33,48 @@ function ProfileMenu() {
       )}
 
       <div className={`absolute top-full right-0 bg-white rounded-md shadow-lg overflow-hidden z-50 min-w-[160px] transform ${dropdownVisible ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0'} transition-all duration-300 ease-in-out`}>
-        {isAuthenticated && user ? (
-          <>
+        <SignedIn>
+          {user && (
             <div className="p-4 border-b border-gray-200">
-              <p className="font-bold mb-1">{`${user.firstName || 'User'} ${user.lastName || ''}`}</p>
-              <p className="text-sm text-gray-500">{user.email || 'No email available'}</p>
+              <p className="font-bold mb-1">{user.username || ""}</p>
+              <p className="text-sm text-gray-500">{user.emailAddresses?.[0]?.emailAddress || 'No email available'}</p>
             </div>
+          )}
 
-            <Link to='/profile' className="px-4 py-3 hover:bg-teal-100 flex items-center">
-              < FaUser className='w-[20px] h-[20px] mr-2' />
-              My Profile
-            </Link>
 
-            <Link to='/create-blog' className="flex items-center px-4 py-3 hover:bg-teal-100">
-              < FaPen className='w-[20px] h-[20px] mr-2' />
-              Create Blog
-            </Link>
+          <Link to='/profile' className="px-4 py-3 hover:bg-teal-100 flex items-center">
+            < FaUser className='w-[20px] h-[20px] mr-2' />
+            My Profile
+          </Link>
 
-            <Link to='/settings' className="flex items-center px-4 py-3 hover:bg-teal-100">
-              < FaCog className='w-[20px] h-[20px] mr-2' />
-              Settings
-            </Link>
+          <Link to='/create-blog' className="flex items-center px-4 py-3 hover:bg-teal-100">
+            < FaPen className='w-[20px] h-[20px] mr-2' />
+            Create Blog
+          </Link>
 
-            <button 
-              onClick={handleOpenLogout} 
-              className="flex items-center w-full text-left px-4 py-3 hover:bg-teal-100">
-              < FaSignOutAlt className='w-[20px] h-[20px] mr-2' />
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link to='/login' className="block px-4 py-3 hover:bg-teal-100">Login</Link>
-            <Link to='/signup' className="block px-4 py-3 hover:bg-teal-100">Signup</Link>
-          </>
-        )}
+          <Link to='/settings' className="flex items-center px-4 py-3 hover:bg-teal-100">
+            < FaCog className='w-[20px] h-[20px] mr-2' />
+            Settings
+          </Link>
+
+          <button
+            onClick={handleOpenLogout}
+            className="flex items-center w-full text-left px-4 py-3 hover:bg-teal-100">
+            < FaSignOutAlt className='w-[20px] h-[20px] mr-2' />
+            Logout
+          </button>
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton>
+            <div className='block px-4 py-3 hover:bg-teal-100'>Login</div>
+          </SignInButton>
+
+          <SignUpButton>
+            <button className='block px-4 py-3 hover:bg-teal-100'>Signup</button>
+          </SignUpButton>
+        </SignedOut>
+
       </div>
 
       <Logout open={openLogout} handleClose={handleCloseLogout} />
